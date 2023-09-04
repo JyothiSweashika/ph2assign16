1ca182efceaf457c2f812a4fcd2b949b8e72dcb0
JyothiSweashika <jyothisweashikaraavi@gmail.com>
2023-09-04 16:10:42 +0530

    assignment16

---
+++ Data/WebApplication3DbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication3.Models;
+
+namespace WebApplication3.Data
+{
+    public class WebApplication3DbContext : DbContext
+    {
+        public WebApplication3DbContext (DbContextOptions<WebApplication3DbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication3.Models.Task> Task { get; set; } = default!;
+    }
+}
